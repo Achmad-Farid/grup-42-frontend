dc9e4005678636d3547b9f46d4184153727d4e68
farid <valkanid12@gmail.com>
2024-06-06 23:08:39 +0700

    link

--- src/redux/actions/detailActions.js
+++ src/redux/actions/detailActions.js
@@ -38,7 +38,7 @@ export function getWebinarById(id) {
   return async function (dispatch) {
     dispatch(fetching());
     try {
-      const { data } = await axios(`19.81.65.99:3000/webinar/${id}`);
+      const { data } = await axios(`https://backend-grup42.my.id/webinar/${id}`);
       dispatch(success(data));
     } catch (error) {
       console.error("Failed to fetch webinar by ID:", error);
